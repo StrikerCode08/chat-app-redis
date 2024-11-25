ee7f1843368b8c3d13ef313a946625b7ae941971
akash <akash@poshn.co>
2024-11-25 13:44:10 +0530

    package-lock

--- server/dist/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authControllers_1 = require("../controllers/authControllers");
-const router = (0, express_1.Router)();
-router.post("/register", authControllers_1.registerUser);
-router.post("/login", authControllers_1.loginUser);
-exports.default = router;
